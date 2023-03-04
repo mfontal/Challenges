7e47853ea7a2740e731ff1f0c77e3bf01ab15457
mfontal <58881548+mfontal@users.noreply.github.com>
2023-03-04 05:11:53 -0500

    challenge-4

---
+++ src/Challenge2.jsx
@@ -0,0 +1,10 @@
+const Challenge2 = () => {
+    return (
+      <>
+        <h1> Hola Mundo </h1>
+        <span style={{fontSize: '10px', color: 'red'}}> 10 </span>
+      </>
+    );
+  }
+  
+  export default Challenge2;
